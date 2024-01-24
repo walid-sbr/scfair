2d9ede4114b90bb135a98af63c2187692177c01d
walid-sbr <sabeur.walid@gmail.com>
2024-01-24 09:40:33 +0100

    changed db schema

--- src/db/migrate/20240124064152_create_datasets.rb
+++ src/db/migrate/20240124064152_create_datasets.rb
@@ -1,6 +1,9 @@
 class CreateDatasets < ActiveRecord::Migration[7.0]
   def change
     create_table :datasets do |t|
+
+      t.string :collection_id # Id of the collection containing Datatsets
+      t.string :dataset_id, unique: true # Id du dataset
       t.string :source  # Source of the API (CellXGene, Bgee, ASAP...)
       t.string :doi, null: true # Digital Object Identifier of scientific paper
       t.string :cell_types, array: true, default: []  # Array of cell types of the dataset
@@ -9,14 +12,14 @@ class CreateDatasets < ActiveRecord::Migration[7.0]
       t.string :developmental_stage, array: true, default: [] # Array of developmental stages
       t.string :developmental_stage_id, array: true, default: [] # Array of developmental stages id
       t.string :sex, array: true, default: [] # Array of sex
-      t.string :organisms, null: true  # Organism label
-      t.string :disease, null: true  # Disease status
-      t.string :assay_info, null: true  # Assay type
-      t.integer :number_of_cells, null: true  # Number of cells
-      t.string :processed_data, null: true  # Link to download processed dataset
+      t.string :organisms, array: true, default: [] # Organism label
+      t.string :disease, array: true, default: [] # Disease status
+      t.string :assay_info, array: true, default: []  # Assay type
+      t.integer :number_of_cells, array: true, default: []  # Number of cells
+      t.string :processed_data, array: true, default: []  # Link to download processed dataset
       t.string :link_to_dataset, null: true  # Link to collection url
-      t.string :link_to_explore_data, null: true   # Link to explore dataset
-      t.string :link_to_raw_data, null: true  # Link to download raw dataset
+      t.string :link_to_explore_data, array: true, default: []   # Link to explore dataset
+      t.string :link_to_raw_data, array: true, default: []   # Link to download raw dataset
       t.string :hash # Hash of JSON to check if changes
 
       t.timestamps
