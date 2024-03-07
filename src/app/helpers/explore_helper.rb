a40b75b8fe589430c86204336626e99a4471f93f
walid-sbr <sabeur.walid@gmail.com>
2024-03-07 10:56:04 +0100

    adapte pagination

--- src/app/helpers/explore_helper.rb
+++ src/app/helpers/explore_helper.rb
@@ -1,12 +1,14 @@
 module ExploreHelper
 
   def get_pagination(current_page, number_of_rows, per_page)
-    if (current_page - per_page) <= 1
+    if (number_of_rows <= (per_page * 10))
+      return(1..((number_of_rows / per_page) + 1))
+    elsif (current_page - 5) <= 1
       return(1..10)
-    elsif (current_page + per_page) > (number_of_rows / per_page)
-      return((current_page - 10)..(current_page))
+    elsif (current_page + 5) > (number_of_rows / per_page)
+      return(current_page - 10)..(current_page)
     else
-      return((current_page - per_page)..(current_page + per_page))
+      return((current_page - 5)..(current_page + 5))
     end
   end
 
