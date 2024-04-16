5a6912d462b71c0a119f2b7703cec92565c942b0
walid-sbr <sabeur.walid@gmail.com>
2024-04-16 09:42:44 +0200

    adding styling of table

--- src/app/controllers/datasets_controller.rb
+++ src/app/controllers/datasets_controller.rb
@@ -1,60 +1,53 @@
 class DatasetsController < ApplicationController
   before_action :set_dataset, only: %i[ show edit update destroy ]
+  helper_method [:ontology_link_generator]
 
   # GET /datasets or /datasets.json
   def index
+
+    # Hash of source sites links
+    @sources_link = {
+      "CELLxGENE": 'https://cellxgene.cziscience.com/',
+      "BGEE": "https://www.bgee.org/"
+    }
+
+    @fields = {
+      "Number of Cells": {
+        name: :number_of_cells,
+      },
+      "Species": {
+        name: :organisms,
+      },
+      "Disease": {
+        name: :disease,
+      },
+      "Assays": {
+        name: :assay_info,
+      },
+      "Cell Type": {
+        name: :cell_types,
+      },
+      "Sex": {
+        name: :sex,
+      },
+      "Tissue": {
+        name: :tissue,
+      },
+      "Tissue ID": {
+        name: :tissue_uberon,
+      },
+      "Developmental Stage": {
+        name: :developmental_stage,
+      },
+      "Developmental Stage ID": {
+        name: :developmental_stage_id,
+      }
+    }
     @datasets = Dataset.all
   end
 
   # GET /datasets/1 or /datasets/1.json
   def show
-  end
-
-  # GET /datasets/new
-  def new
-    @dataset = Dataset.new
-  end
-
-  # GET /datasets/1/edit
-  def edit
-  end
-
-  # POST /datasets or /datasets.json
-  def create
-    @dataset = Dataset.new(dataset_params)
-
-    respond_to do |format|
-      if @dataset.save
-        format.html { redirect_to dataset_url(@dataset), notice: "Dataset was successfully created." }
-        format.json { render :show, status: :created, location: @dataset }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @dataset.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /datasets/1 or /datasets/1.json
-  def update
-    respond_to do |format|
-      if @dataset.update(dataset_params)
-        format.html { redirect_to dataset_url(@dataset), notice: "Dataset was successfully updated." }
-        format.json { render :show, status: :ok, location: @dataset }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @dataset.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /datasets/1 or /datasets/1.json
-  def destroy
-    @dataset.destroy
-
-    respond_to do |format|
-      format.html { redirect_to datasets_url, notice: "Dataset was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -66,5 +59,11 @@ class DatasetsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def dataset_params
       params.fetch(:dataset, {})
+    end
+
+    def ontology_link_generator(type, id)
+      id = id.sub ":", "_" # necessary beacause the url uses _ instead of : in the id
+      url = "https://www.ebi.ac.uk/ols4/ontologies/#{type}/classes/http%253A%252F%252Fpurl.obolibrary.org%252Fobo%252F#{id}"
+      return(url)
     end
 end
