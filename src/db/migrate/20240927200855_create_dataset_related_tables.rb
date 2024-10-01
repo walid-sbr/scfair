92b9f288a85aa2455c7868dcd3590798f606ed00
luxxi <luka.domitrovic@livestorm.co>
2024-10-01 22:39:22 +0200

    update migration

--- src/db/migrate/20240927200855_create_dataset_related_tables.rb
+++ src/db/migrate/20240927200855_create_dataset_related_tables.rb
@@ -1,119 +1,134 @@
 class CreateDatasetRelatedTables < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto' # Enable the pgcrypto extension for UUID generation
+    rename_table :datasets, :datasets_old
+
+    enable_extension "uuid-ossp"
 
     create_table :datasets, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :collection_id
-      t.string :dataset_id
-      t.string :source_name
-      t.string :source_url # previously link_to_dataset
-      t.string :data_explorer_url # previously link_to_explore_data
+      t.string :collection_id, null: false
+      t.string :source_reference_id, null: false
+      t.string :source_name, null: false, index: true
+      t.string :source_url, null: false # previously link_to_dataset
+      t.string :explorer_url, null: false # previously link_to_explore_data
       t.string :doi
-      t.integer :number_of_cells
-      t.string :parser_hash
+      t.integer :cell_count, null: false, default: 0, index: true
+      t.string :parser_hash, null: false
       t.timestamps
     end
 
     create_table :sexes, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :ontology_term_id, index: true
       t.timestamps
+
+      t.index :name, unique: true
     end
 
     create_table :datasets_sexes, id: false, force: :cascade do |t|
       t.references :dataset, type: :uuid, null: false, foreign_key: true
       t.references :sex, type: :uuid, null: false, foreign_key: true
+
       t.index [:dataset_id, :sex_id], unique: true
     end
 
     create_table :cell_types, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :ontology_term_id, index: true
       t.timestamps
+
+      t.index :name, unique: true
     end
 
-    create_table :datasets_cell_types, id: false, force: :cascade do |t|
+    create_table :cell_types_datasets, id: false, force: :cascade do |t|
       t.references :dataset, type: :uuid, null: false, foreign_key: true
       t.references :cell_type, type: :uuid, null: false, foreign_key: true
+
       t.index [:dataset_id, :cell_type_id], unique: true
     end
 
     create_table :tissues, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :ontology_term_id, index: true
       t.timestamps
+
+      t.index :name, unique: true
     end
 
     create_table :datasets_tissues, id: false, force: :cascade do |t|
       t.references :dataset, type: :uuid, null: false, foreign_key: true
       t.references :tissue, type: :uuid, null: false, foreign_key: true
+
       t.index [:dataset_id, :tissue_id], unique: true
     end
 
     create_table :developmental_stages, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :ontology_term_id, index: true
       t.timestamps
+
+      t.index :name, unique: true
     end
 
     create_table :datasets_developmental_stages, id: false, force: :cascade do |t|
       t.references :dataset, type: :uuid, null: false, foreign_key: true
       t.references :developmental_stage, type: :uuid, null: false, foreign_key: true
-      t.index [:dataset_id, :developmental_stage_id], unique: true
+
+      t.index [:dataset_id, :developmental_stage_id], unique: true, name: "index_datasets_dev_stages_on_dataset_id_and_dev_stage_id"
     end
 
     create_table :organisms, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :ontology_term_id, index: true
       t.timestamps
+
+      t.index :name, unique: true
     end
 
     create_table :datasets_organisms, id: false, force: :cascade do |t|
       t.references :dataset, type: :uuid, null: false, foreign_key: true
       t.references :organism, type: :uuid, null: false, foreign_key: true
+
       t.index [:dataset_id, :organism_id], unique: true
     end
 
     create_table :diseases, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :ontology_term_id, index: true
       t.timestamps
+
+      t.index :name, unique: true
     end
 
     create_table :datasets_diseases, id: false, force: :cascade do |t|
       t.references :dataset, type: :uuid, null: false, foreign_key: true
       t.references :disease, type: :uuid, null: false, foreign_key: true
+
       t.index [:dataset_id, :disease_id], unique: true
     end
 
-    create_table :assay_info, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :info
+    create_table :technologies, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+      t.string :protocol_name, null: false
       t.string :ontology_term_id, index: true
       t.timestamps
+
+      t.index :protocol_name, unique: true
     end
 
-    create_table :datasets_assay_info, id: false, force: :cascade do |t|
+    create_table :datasets_technologies, id: false, force: :cascade do |t|
       t.references :dataset, type: :uuid, null: false, foreign_key: true
-      t.references :assay_info, type: :uuid, null: false, foreign_key: true
-      t.index [:dataset_id, :assay_info_id], unique: true
+      t.references :technology, type: :uuid, null: false, foreign_key: true
+
+      t.index [:dataset_id, :technology_id], unique: true
     end
 
     # previously processed data
-    create_table :files, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-      t.string :url
+    create_table :file_resources, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+      t.references :dataset, type: :uuid, null: false, foreign_key: true
+      t.string :url, null: false
+      t.integer :filetype, default: 0
       t.timestamps
     end
 
-    create_table :datasets_files, id: false, force: :cascade do |t|
-      t.references :dataset, type: :uuid, null: false, foreign_key: true
-      t.references :file, type: :uuid, null: false, foreign_key: true
-      t.index [:dataset_id, :file_id], unique: true
-    end
-
-    create_table :datasets_ext_sources, id: false, force: :cascade do |t|
-      t.references :dataset, type: :uuid, null: false, foreign_key: true
-      t.references :ext_source, type: :uuid, null: false, foreign_key: true
-      t.index [:dataset_id, :ext_source_id], unique: true
-    end
+    add_reference :ext_sources, :dataset, null: false, index: true
   end
 end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,11 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_23_185307) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_27_200855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
-  create_table "datasets", force: :cascade do |t|
+  create_table "cell_types", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ontology_term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_cell_types_on_name", unique: true
+    t.index ["ontology_term_id"], name: "index_cell_types_on_ontology_term_id"
+  end
+
+  create_table "cell_types_datasets", id: false, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.uuid "cell_type_id", null: false
+    t.index ["cell_type_id"], name: "index_cell_types_datasets_on_cell_type_id"
+    t.index ["dataset_id", "cell_type_id"], name: "index_cell_types_datasets_on_dataset_id_and_cell_type_id", unique: true
+    t.index ["dataset_id"], name: "index_cell_types_datasets_on_dataset_id"
+  end
+
+  create_table "datasets", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "collection_id", null: false
+    t.string "source_reference_id", null: false
+    t.string "source_name", null: false
+    t.string "source_url", null: false
+    t.string "explorer_url", null: false
+    t.string "doi"
+    t.integer "cell_count", default: 0, null: false
+    t.string "parser_hash", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cell_count"], name: "index_datasets_on_cell_count"
+    t.index ["source_name"], name: "index_datasets_on_source_name"
+  end
+
+  create_table "datasets_developmental_stages", id: false, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.uuid "developmental_stage_id", null: false
+    t.index ["dataset_id", "developmental_stage_id"], name: "index_datasets_dev_stages_on_dataset_id_and_dev_stage_id", unique: true
+    t.index ["dataset_id"], name: "index_datasets_developmental_stages_on_dataset_id"
+    t.index ["developmental_stage_id"], name: "index_datasets_developmental_stages_on_developmental_stage_id"
+  end
+
+  create_table "datasets_diseases", id: false, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.uuid "disease_id", null: false
+    t.index ["dataset_id", "disease_id"], name: "index_datasets_diseases_on_dataset_id_and_disease_id", unique: true
+    t.index ["dataset_id"], name: "index_datasets_diseases_on_dataset_id"
+    t.index ["disease_id"], name: "index_datasets_diseases_on_disease_id"
+  end
+
+  create_table "datasets_old", force: :cascade do |t|
     t.string "collection_id"
     t.string "dataset_id"
     t.string "source"
@@ -39,6 +88,56 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_23_185307) do
     t.string "ext_source_ids", default: [], array: true
   end
 
+  create_table "datasets_organisms", id: false, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.uuid "organism_id", null: false
+    t.index ["dataset_id", "organism_id"], name: "index_datasets_organisms_on_dataset_id_and_organism_id", unique: true
+    t.index ["dataset_id"], name: "index_datasets_organisms_on_dataset_id"
+    t.index ["organism_id"], name: "index_datasets_organisms_on_organism_id"
+  end
+
+  create_table "datasets_sexes", id: false, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.uuid "sex_id", null: false
+    t.index ["dataset_id", "sex_id"], name: "index_datasets_sexes_on_dataset_id_and_sex_id", unique: true
+    t.index ["dataset_id"], name: "index_datasets_sexes_on_dataset_id"
+    t.index ["sex_id"], name: "index_datasets_sexes_on_sex_id"
+  end
+
+  create_table "datasets_technologies", id: false, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.uuid "technology_id", null: false
+    t.index ["dataset_id", "technology_id"], name: "index_datasets_technologies_on_dataset_id_and_technology_id", unique: true
+    t.index ["dataset_id"], name: "index_datasets_technologies_on_dataset_id"
+    t.index ["technology_id"], name: "index_datasets_technologies_on_technology_id"
+  end
+
+  create_table "datasets_tissues", id: false, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.uuid "tissue_id", null: false
+    t.index ["dataset_id", "tissue_id"], name: "index_datasets_tissues_on_dataset_id_and_tissue_id", unique: true
+    t.index ["dataset_id"], name: "index_datasets_tissues_on_dataset_id"
+    t.index ["tissue_id"], name: "index_datasets_tissues_on_tissue_id"
+  end
+
+  create_table "developmental_stages", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ontology_term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_developmental_stages_on_name", unique: true
+    t.index ["ontology_term_id"], name: "index_developmental_stages_on_ontology_term_id"
+  end
+
+  create_table "diseases", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ontology_term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_diseases_on_name", unique: true
+    t.index ["ontology_term_id"], name: "index_diseases_on_ontology_term_id"
+  end
+
   create_table "ext_sources", force: :cascade do |t|
     t.string "url_mask"
     t.string "name"
@@ -46,6 +145,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_23_185307) do
     t.string "id_regexp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "dataset_id", null: false
+    t.index ["dataset_id"], name: "index_ext_sources_on_dataset_id"
+  end
+
+  create_table "file_resources", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.uuid "dataset_id", null: false
+    t.string "url", null: false
+    t.integer "filetype", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dataset_id"], name: "index_file_resources_on_dataset_id"
   end
 
   create_table "journals", force: :cascade do |t|
@@ -65,6 +175,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_23_185307) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["identifier"], name: "index_ontology_terms_on_identifier", unique: true
+  end
+
+  create_table "organisms", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ontology_term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_organisms_on_name", unique: true
+    t.index ["ontology_term_id"], name: "index_organisms_on_ontology_term_id"
+  end
+
+  create_table "sexes", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ontology_term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_sexes_on_name", unique: true
+    t.index ["ontology_term_id"], name: "index_sexes_on_ontology_term_id"
   end
 
   create_table "studies", force: :cascade do |t|
@@ -87,6 +215,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_23_185307) do
     t.index ["journal_id"], name: "index_studies_on_journal_id"
   end
 
+  create_table "technologies", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "protocol_name", null: false
+    t.string "ontology_term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ontology_term_id"], name: "index_technologies_on_ontology_term_id"
+    t.index ["protocol_name"], name: "index_technologies_on_protocol_name", unique: true
+  end
+
+  create_table "tissues", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ontology_term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tissues_on_name", unique: true
+    t.index ["ontology_term_id"], name: "index_tissues_on_ontology_term_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -99,5 +245,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_23_185307) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cell_types_datasets", "cell_types"
+  add_foreign_key "cell_types_datasets", "datasets"
+  add_foreign_key "datasets_developmental_stages", "datasets"
+  add_foreign_key "datasets_developmental_stages", "developmental_stages"
+  add_foreign_key "datasets_diseases", "datasets"
+  add_foreign_key "datasets_diseases", "diseases"
+  add_foreign_key "datasets_organisms", "datasets"
+  add_foreign_key "datasets_organisms", "organisms"
+  add_foreign_key "datasets_sexes", "datasets"
+  add_foreign_key "datasets_sexes", "sexes"
+  add_foreign_key "datasets_technologies", "datasets"
+  add_foreign_key "datasets_technologies", "technologies"
+  add_foreign_key "datasets_tissues", "datasets"
+  add_foreign_key "datasets_tissues", "tissues"
+  add_foreign_key "file_resources", "datasets"
   add_foreign_key "studies", "journals"
 end
