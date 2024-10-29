9862440b819fd0cc0a80f8e18dac7d342c7146b4
luxxi <luka.domitrovic@livestorm.co>
2024-10-29 19:06:52 +0100

    add asap parser

---
+++ src/app/parsers/asap_parser.rb
@@ -0,0 +1,121 @@
+class AsapParser
+  BASE_URL = "https://asap.epfl.ch/projects.json"
+
+  attr_reader :errors
+
+  def initialize
+    @errors = []
+  end
+
+  def perform
+    fetch_collections.each do |collection_data|
+      process_dataset(collection_data)
+    end
+
+    @errors.empty?
+  end
+
+  private
+
+  def fetch_collections
+    response = HTTParty.get(BASE_URL)
+    JSON.parse(response.body, symbolize_names: true)
+  rescue => e
+    @errors << "Error fetching collections: #{e.message}"
+    []
+  end
+
+  def process_dataset(data)
+    parser_hash = Digest::SHA256.hexdigest(data.to_s)
+    dataset = Dataset.find_or_initialize_by(source_reference_id: data[:public_key])
+    
+    return if dataset.parser_hash == parser_hash
+
+    dataset_data = {
+      collection_id: "ASAP000000",
+      source_name: "ASAP",
+      source_url: "https://asap.epfl.ch/projects/#{data[:public_key]}",
+      explorer_url: "",
+      doi: data[:doi],
+      cell_count: data[:nber_cols],
+      parser_hash: parser_hash
+    }
+
+    dataset.assign_attributes(dataset_data)
+
+    if dataset.save
+      update_cell_types(dataset, extract_cell_types(data))
+      update_organisms(dataset, [data[:organism]].compact)
+      update_technologies(dataset, [data[:technology]].compact)
+      update_file_resources(dataset, extract_files(data))
+      
+      puts "Imported #{dataset.id}"
+    else
+      @errors << "Failed to save dataset #{data[:public_key]}: #{dataset.errors.full_messages.join(', ')}"
+    end
+  end
+
+  def extract_cell_types(data)
+    data[:annotation_groups].flat_map do |group|
+      group[:annotations].flat_map do |annotation|
+        annotation[:cell_ontology_terms].map { |term| term[:name] }
+      end
+    end.compact.uniq
+  end
+
+  def extract_files(data)
+    files = []
+    
+    data[:experiments].each do |experiment|
+      files << { url: experiment[:url], filetype: "raw" }
+    end
+    
+    files << {
+      url: "https://asap.epfl.ch/projects/#{data[:key]}/get_file?filename=parsing/output.h5ad",
+      filetype: "h5ad"
+    }
+    
+    files
+  end
+
+  def update_cell_types(dataset, cell_types)
+    dataset.cell_types.clear
+    cell_types.each do |cell_type|
+      next if cell_type.blank?
+      
+      cell_type_record = CellType.find_or_create_by(name: cell_type)
+      dataset.cell_types << cell_type_record unless dataset.cell_types.include?(cell_type_record)
+    end
+  end
+
+  def update_organisms(dataset, organisms)
+    dataset.organisms.clear
+    organisms.each do |organism|
+      next if organism.blank?
+      
+      organism_record = Organism.find_or_create_by(name: organism)
+      dataset.organisms << organism_record unless dataset.organisms.include?(organism_record)
+    end
+  end
+
+  def update_technologies(dataset, technologies)
+    dataset.technologies.clear
+    technologies.each do |technology|
+      next if technology.blank?
+      
+      technology_record = Technology.find_or_create_by(protocol_name: technology)
+      dataset.technologies << technology_record unless dataset.technologies.include?(technology_record)
+    end
+  end
+
+  def update_file_resources(dataset, files)
+    files.each do |file|
+      next if file[:url].blank?
+      
+      dataset.file_resources.find_or_create_by(
+        url: file[:url],
+        filetype: Dataset::FILETYPES[file[:filetype]] || :undefined
+      )
+    end
+  end
+end 
---
+++ src/spec/parsers/asap_parser_spec.rb
@@ -0,0 +1,101 @@
+require "rails_helper"
+
+RSpec.describe AsapParser do
+  let(:parser) { described_class.new }
+  
+  let(:dataset_data) do
+    {
+      public_key: "test123",
+      doi: "10.1234/test",
+      nber_cols: 1000,
+      organism: "human",
+      technology: "RNA-seq",
+      key: "test123",
+      experiments: [
+        { url: "http://example.com/acc1.cgi" },
+        { url: "http://example.com/acc2.cgi" }
+      ],
+      annotation_groups: [
+        {
+          annotations: [
+            {
+              cell_ontology_terms: [
+                { name: "T cell" },
+                { name: "B cell" }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  end
+
+  describe "#perform" do
+    before do
+      allow(HTTParty).to receive(:get).with(described_class::BASE_URL)
+        .and_return(double(body: [dataset_data].to_json))
+    end
+
+    it "processes collections successfully" do
+      expect(parser.perform).to be true
+      expect(parser.errors).to be_empty
+    end
+
+    context "when there's an error fetching collections" do
+      before do
+        allow(HTTParty).to receive(:get)
+          .and_raise(StandardError.new("API Error"))
+      end
+
+      it "handles the error gracefully" do
+        expect(parser.perform).to be false
+        expect(parser.errors).to include("Error fetching collections: API Error")
+      end
+    end
+  end
+
+  describe "#process_dataset" do
+    it "creates a new dataset with associated records" do
+      expect {
+        parser.send(:process_dataset, dataset_data)
+      }.to change(Dataset, :count).by(1)
+
+      dataset = Dataset.last
+      expect(dataset.source_name).to eq("ASAP")
+      expect(dataset.doi).to eq("10.1234/test")
+      expect(dataset.cell_count).to eq(1000)
+      
+      expect(dataset.cell_types.pluck(:name)).to contain_exactly("T cell", "B cell")
+      expect(dataset.organisms.pluck(:name)).to contain_exactly("human")
+      expect(dataset.technologies.pluck(:protocol_name)).to contain_exactly("RNA-seq")
+      expect(dataset.file_resources.count).to eq(3) # 2 raw files + 1 processed
+    end
+
+    it "updates existing dataset when parser_hash changes" do
+      parser.send(:process_dataset, dataset_data)
+      initial_dataset = Dataset.last
+      initial_hash = initial_dataset.parser_hash
+
+      modified_data = dataset_data.deep_dup
+      modified_data[:technology] = "ATAC-seq"
+      
+      parser.send(:process_dataset, modified_data)
+      
+      updated_dataset = Dataset.last
+      expect(updated_dataset.id).to eq(initial_dataset.id)
+      expect(updated_dataset.parser_hash).not_to eq(initial_hash)
+      expect(updated_dataset.technologies.pluck(:protocol_name)).to contain_exactly("ATAC-seq")
+    end
+
+    it "skips processing when parser_hash hasn't changed" do
+      parser.send(:process_dataset, dataset_data)
+      initial_dataset = Dataset.last
+      
+      expect {
+        parser.send(:process_dataset, dataset_data)
+      }.not_to change(Dataset, :count)
+      
+      expect(Dataset.last.parser_hash).to eq(initial_dataset.parser_hash)
+    end
+  end
+end 
