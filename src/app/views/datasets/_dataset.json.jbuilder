5c5577ebe1a68978b4955dbe80f42db7d2cc0e53
luxxi <luka.domitrovic@livestorm.co>
2024-11-01 11:05:50 +0100

    rebuild dataset views

--- src/app/views/datasets/_dataset.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! dataset, :id, :created_at, :updated_at
-json.url dataset_url(dataset, format: :json)
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,29 +2,16 @@ Rails.application.routes.draw do
   resources :ext_sources
   resources :studies
   resources :journals
-  resources :datasets do
-    collection do
-      post :search
-      get :search2
-      get :index2
-      post :do_search
-      post :set_search_session
-    end
-  end
+  resources :datasets, only: :index, path: "explore"
+
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  root to: 'home#index'
-
+  
   get 'tools', to: 'home#tools', as: :tools
   get 'contact-us', to: 'home#contact', as: :contact
   get 'resources', to: 'home#resources', as: :resources
   get 'metadata-schema', to: 'home#metadata_schema', as: :metadata_schema
   get 'about', to: 'home#about', as: :about
   get 'community', to: 'home#community', as: :community
-  get 'explore', to: 'datasets#index', as: :explore
 
+  root to: 'home#index'
 end
